a21b32bde484d7ca56b79ad2f6e5d14e5724ef3a
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2023-01-24 09:31:00 -0800

    Create 2023-01-24_rfecv-vs-lasso-for-feature-selection.py

---
+++ src/2023-01-24_rfecv-vs-lasso-for-feature-selection.py
@@ -0,0 +1,3 @@
+# - https://machinelearningmastery.com/rfe-feature-selection-in-python/
+# - https://www.scikit-yb.org/en/latest/api/model_selection/rfecv.html
+# - https://twitter.com/baddatasciencer/status/1617510422508077057
