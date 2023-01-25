5df2b0df155c269e0fc7aed36dffeee6c58c0bf5
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2023-01-25 05:27:04 -0800

    Using multiple metrics with sklearn `cross_validate( )`

--- src/2023-01-24_micro-macro-and-weighted-scores.py
+++ src/2023-01-24_micro-macro-and-weighted-scores.py
@@ -11,8 +11,7 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split, cross_val_score, cross_validate
 from sklearn.linear_model import LogisticRegressionCV
 from sklearn.svm import LinearSVC
-from sklearn.metrics import classification_report
-
+import pandas as pd
 
 X, y = load_iris(return_X_y=True)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, shuffle=True)
@@ -24,11 +23,28 @@ lr = LogisticRegressionCV(max_iter=1e8)
 lr.fit(X_train, y_train)
 
 cross_val_score(lr, X_train, y_train, scoring="accuracy").mean()
-cross_validate(lr, X_train, y_train, scoring=("accuracy", "precision"))
-classification_report(lr, X_train, y_train)
+pd.DataFrame(
+    cross_validate(
+        lr,
+        X_train,
+        y_train,
+        return_train_score=True,
+        scoring=(
+            "precision_micro",
+            "precision_macro",
+            "precision_weighted",
+            "recall_micro",
+            "recall_macro",
+            "recall_weighted",
+            "f1_micro",
+            "f1_macro",
+            "f1_weighted",
+        ),
+    )
+)
 
 # SVC
-svc = LinearSVC
+svc = LinearSVC()
 
 # Selected model = _
 
