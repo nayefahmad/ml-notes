8353ce96974cbec1fcb60467973b1296c2f7a7b5
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2023-01-26 18:21:33 -0800

    Create gensim_word_embeddings

---
+++ src/2023-01-26_gensim-word-embeddings.py
@@ -0,0 +1,3 @@
+# [kaggle nb](https://www.kaggle.com/code/chewzy/tutorial-how-to-train-your-custom-word-embedding)  # noqa
+
+# [ML mastery](https://machinelearningmastery.com/develop-word-embeddings-python-gensim/)  # noqa
