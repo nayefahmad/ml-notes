c1eac2df3460924a99ec88ceb7ca005293eac8dd
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2023-05-29 17:36:54 -0700

    Update 2023-01-24_micro-macro-and-weighted-scores.py
    Clean up

--- src/2023-01-24_micro-macro-and-weighted-scores.py
+++ src/2023-01-24_micro-macro-and-weighted-scores.py
@@ -5,49 +5,36 @@
 # ## References
 
 # - [sklearn docs](https://scikit-learn.org/stable/modules/model_evaluation.html#classification-metrics)  # noqa
+# - [SE post](https://datascience.stackexchange.com/questions/15989/micro-average-vs-macro-average-performance-in-a-multiclass-classification-settin)  # noqa
 
 
-from sklearn.datasets import load_iris
-from sklearn.model_selection import train_test_split, cross_val_score, cross_validate
-from sklearn.linear_model import LogisticRegressionCV
-from sklearn.svm import LinearSVC
 import pandas as pd
+from sklearn.datasets import load_iris
+from sklearn.linear_model import LogisticRegressionCV
+from sklearn.metrics import (
+    classification_report,
+    precision_recall_fscore_support,
+    precision_score,
+    recall_score,
+)
+from sklearn.model_selection import train_test_split
 
 X, y = load_iris(return_X_y=True)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, shuffle=True)
 
-# ## Fit two models on training data and use CV scores to select a final model
-
-# LR
 lr = LogisticRegressionCV(max_iter=1e8)
 lr.fit(X_train, y_train)
+preds = lr.predict(X_train)
 
-cross_val_score(lr, X_train, y_train, scoring="accuracy").mean()
-pd.DataFrame(
-    cross_validate(
-        lr,
-        X_train,
-        y_train,
-        return_train_score=True,
-        scoring=(
-            "precision_micro",
-            "precision_macro",
-            "precision_weighted",
-            "recall_micro",
-            "recall_macro",
-            "recall_weighted",
-            "f1_micro",
-            "f1_macro",
-            "f1_weighted",
-        ),
-    )
+scores = precision_recall_fscore_support(y_train, preds)
+pd.DataFrame(scores).T.reset_index().rename(
+    columns={0: "precision", 1: "recall", 2: "f1", 3: "support", "index": "class"}
 )
 
-# SVC
-svc = LinearSVC()
+print(classification_report(y_train, preds))
 
-# Selected model = _
+micro_precision = precision_score(y_train, preds, average="micro")
+macro_precision = precision_score(y_train, preds, average="macro")
 
-# Evaluate on test set
-
-# Discuss micro-, macro-, weighted-average results
+micro_recall = recall_score(y_train, preds, average="micro")
+macro_recall = recall_score(y_train, preds, average="macro")
