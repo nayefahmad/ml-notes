8948dfd7e4898bebd13f89a11b6a915d9adfde22
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2024-03-22 11:45:37 -0700

    Add nested CV code

--- src/2024-03-20_nested-cv.py
+++ src/2024-03-20_nested-cv.py
@@ -12,16 +12,16 @@ we use nested CV
 """
 
 from sklearn.datasets import load_breast_cancer
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import GridSearchCV, KFold, cross_val_score
 from sklearn.svm import SVC
 
 data, target = load_breast_cancer(return_X_y=True)
 
-model = SVC()
+model_to_tune = SVC()
 
 param_grid = {"C": [0.1, 1, 10], "gamma": [0.01, 0.1]}
 
-search = GridSearchCV(estimator=model, param_grid=param_grid, cv=5, n_jobs=-1)
+search = GridSearchCV(estimator=model_to_tune, param_grid=param_grid, cv=5, n_jobs=-1)
 search.fit(data, target)
 
 print(f"best params: {search.best_params_}")
@@ -46,5 +46,21 @@ error of the model that uses these params?
   hyperparameters selected and used for predicting on the test fold were selected using
   CV on the train set, and trying out several different hyperparameter values.
 """
+
+inner_cv = KFold(n_splits=5, shuffle=True, random_state=2024)
+outer_cv = KFold(n_splits=3, shuffle=True, random_state=2024)
+
+# set up inner CV using GridSearchCV object
+model = GridSearchCV(
+    estimator=model_to_tune, param_grid=param_grid, cv=inner_cv, n_jobs=-1
+)
+
+# Setting cv=outer_cv ensures that a test set is set aside, while the train set
+# that is passed to `model` (aka a GridSearchCV) is then further split using the
+# cv attribute of that object. This ensures that for each of the three outer_cv
+# iterations, the inner CV is used to select best hyperparams, and then the model with
+# those hyperparams is used to predict on the test set from outer_cv, which it has never
+# seen before.
+test_set_scores = cross_val_score(model, data, target, cv=outer_cv, n_jobs=-1)
 
 print("done")
