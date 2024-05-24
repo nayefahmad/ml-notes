1669a5ac386c9d1e1ecd132821943c4f6999be2a
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2024-05-24 12:58:45 -0700

    Train and test

--- src/2024-04-10_pytorch-example.py
+++ src/2024-04-10_pytorch-example.py
@@ -1,6 +1,14 @@
+"""
+Basic example of training a DL model for regression with pytorch
+
+Reference: https://pytorch.org/tutorials/beginner/basics/quickstart_tutorial.html
+
+"""
+
 import numpy as np
 import torch
 from sklearn.datasets import make_regression
+from sklearn.linear_model import LogisticRegressionCV
 from sklearn.model_selection import train_test_split
 from torch import nn
 from torch.utils.data import DataLoader, Dataset
@@ -8,11 +16,13 @@ from torch.utils.data import DataLoader, Dataset
 N_FEATURES = 10
 SEED = 2024
 
-X, y = make_regression(n_samples=400, n_features=N_FEATURES, random_state=SEED)
-X_train, X_test, y_train, y_test = train_test_split(X, y)
-
 
 class TabularData(Dataset):
+    """
+    This class creates objects that store the particular format of data required for our
+    problem.
+    """
+
     def __init__(self, X, y):
         self.X = torch.tensor(X.astype(np.float32))
         self.y = torch.tensor(y.astype(np.float32))
@@ -22,13 +32,6 @@ class TabularData(Dataset):
 
     def __getitem__(self, idx):
         return self.X[idx], self.y[idx]
-
-
-data_train = TabularData(X_train, y_train)
-data_test = TabularData(X_test, y_test)
-
-dataloader_train = DataLoader(data_train, batch_size=8, shuffle=True)
-dataloader_test = DataLoader(data_test, batch_size=8, shuffle=True)
 
 
 class Model(nn.Module):
@@ -45,14 +48,6 @@ class Model(nn.Module):
     def forward(self, x):
         out = self.linear_relu_stack(x)
         return out
-
-
-model = Model()
-print(model)
-
-
-loss_fn = nn.MSELoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
 
 
 def train(dataloader, model, loss_fn, optimizer):
@@ -87,3 +82,31 @@ def test(dataloader, model, loss_fn):
 
     test_loss /= num_batches
     print(f"Avg loss: {test_loss:>8f}")
+
+
+if __name__ == "__main__":
+    X, y = make_regression(n_samples=400, n_features=N_FEATURES, random_state=SEED)
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+    data_train = TabularData(X_train, y_train)
+    data_test = TabularData(X_test, y_test)
+
+    # DataLoader wraps an iterable around a Dataset
+    dataloader_train = DataLoader(data_train, batch_size=8, shuffle=True)
+    dataloader_test = DataLoader(data_test, batch_size=8, shuffle=True)
+
+    model = Model()
+    print(model)
+
+    loss_fn = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+
+    before_training = model.forward(data_train[0][0])
+    train(dataloader_train, model, loss_fn, optimizer)
+    after_training = model.forward(data_train[0][0])
+
+    test(dataloader_test, model, loss_fn)
+
+    # Compare with LR:
+    lrcv = LogisticRegressionCV()
+    # todo: use nested CV to get best h-params, and get generalization error as well
