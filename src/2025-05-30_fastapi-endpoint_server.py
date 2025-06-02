fe66bd98502966b01e5cfc9a110cc33ee2c590e8
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2025-06-02 07:50:20 -0700

    Add endpoints and https file for testing

--- src/2025-05-30_fastapi-endpoint_server.py
+++ src/2025-05-30_fastapi-endpoint_server.py
@@ -4,6 +4,8 @@
 Reference: https://blog.jetbrains.com/pycharm/2024/09/how-to-use-fastapi-for-machine-learning/#what-is-fastapi  # noqa
 """
 
+from typing import Optional
+
 import uvicorn
 from fastapi import FastAPI
 
@@ -13,6 +15,18 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello, {name}"}
+
+
+@app.get("/hello_optional_name")
+async def say_hello_optional_name(name: Optional[str] = None):
+    if name:
+        return {"message": f"Hello, {name}"}
+    return {"message": "Hello, stranger"}
 
 
 if __name__ == "__main__":
